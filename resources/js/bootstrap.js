11119999219182b7f29eb7b3c61bf39433cf9934
José Antonio Henríquez Chavarría <alphyon21@gmail.com>
2024-01-28 10:16:55 -0600

    prepare js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,14 +21,18 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // window.Pusher = Pusher;
 
 import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
-window.Pusher = Pusher;
+
+window.Pusher = require('pusher-js');
+
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    wsHost: import.meta.env.VITE_PUSHER_HOST || `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT || 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT || 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME || 'https') === 'https',
-    enabledTransports: ['ws', 'wss']
+    key: process.env.MIX_PUSHER_APP_KEY,
+    wsHost: process.env.MIX_PUSHER_HOST,
+    wsPort: process.env.MIX_PUSHER_PORT,
+    wssPort: process.env.MIX_PUSHER_PORT,
+    forceTLS: false,
+    encrypted: true,
+    enableLogging: true,
+    disableStats: true,
+    enabledTransports: ['ws', 'wss'],
 });
