584de1e69e8ed83009b9366c789b00eb5923c323
José Antonio Henríquez Chavarría <alphyon21@gmail.com>
2024-01-28 09:28:43 -0600

    base configuration

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class UserController extends Controller
+{
+    public function index(): View
+    {
+        $users = User::paginate();
+
+        return view('users.index', compact('users'));
+    }
+}
